8b5fb24ff304f50cb1f5857521672f57cc4bde1a
Chinta, Raja Srikar Karthik <s530460@nwmissouri.edu>
2019-04-18 17:58:10 -0500

    Work on axions (http requests) 18/04/2019

---
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import axios from 'axios'
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPost: null,
+        error: false
+    }
+    componentDidMount() {
+        axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({
+                    posts: updatedPosts,
+                    error: false
+                })
+            }).catch(err => {
+                this.setState({ error: true })
+            })
+    }
+
+    postClickHandler = (id) => {
+        this.setState({
+            selectedPost: id
+        })
+    }
+
+    render() {
+
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.setState.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postClickHandler(post.id)}
+                />
+            })
+        }
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPost} />
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
